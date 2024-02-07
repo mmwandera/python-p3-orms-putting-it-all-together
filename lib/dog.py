7602d37e751e89595755081d18ff3975f9aa9a2f
mmwandera <mmwandera74@gmail.com>
2024-02-07 21:58:48 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -5,4 +5,148 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    pass
+    # Class Dog in dog.py initializes with name and breed attributes.
+    def __init__(self, name, breed, id=None):
+        self.id = id
+        self.name = name
+        self.breed = breed
+
+    # Class Dog in dog.py contains method "create_table()" that creates table "dogs" if it does not exist.
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs
+                (id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT)
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    # Class Dog in dog.py contains method "drop_table()" that drops table "dogs" if it exists.
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS dogs
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    # Class Dog in dog.py contains method "save()"
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+    
+    # Class Dog in dog.py contains method "create()" that creates a new row in the database and returns a Dog instance.
+    @classmethod
+    def create(cls, name, breed):
+        dog = cls(name, breed)
+        dog.save()
+        return dog
+    
+    # Class Dog in dog.py contains method "new_from_db()" that takes a database row and creates a Dog instance.
+    @classmethod
+    def new_from_db(cls, row):
+        dog = cls(
+            name=row[1],
+            breed=row[2],
+            id=row[0]
+        )
+
+        return dog
+
+    # Class Dog in dog.py contains method "get_all()" that returns a list of Dog instances for every record in the database
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT * FROM dogs
+        """
+
+        return [cls.new_from_db(row) for row in CURSOR.execute(sql).fetchall()]
+    
+    # Class Dog in dog.py contains method "find_by_name()" that returns a Dog instance corresponding to its database record retrieved by name.
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT * FROM dogs
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        if not row:
+            return None
+
+        return Dog(
+            name=row[1],
+            breed=row[2],
+            id=row[0]
+        )
+    
+    # Class Dog in dog.py contains method "find_by_id()" that returns a Dog instance corresponding to its database record retrieved by id.
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * FROM dogs
+            WHERE id = ?
+            LIMIT 1
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        if not row:
+            return None
+
+        return Dog(
+            name=row[1],
+            breed=row[2],
+            id=row[0]
+        )  
+    # Class Dog in dog.py contains method "find_or_create_by()" that takes a name and a breed as arguments and creates a Dog instance matching that record if it does not exist.
+    @classmethod
+    def find_or_create_by(cls, name=None, breed=None):
+        sql = """
+            SELECT * FROM dogs
+            WHERE (name, breed) = (?, ?)
+            LIMIT 1
+        """
+
+        row = CURSOR.execute(sql, (name, breed)).fetchone()
+        if not row:
+            sql = """
+                INSERT INTO dogs (name, breed)
+                VALUES (?, ?)
+            """
+
+            CURSOR.execute(sql, (name, breed))
+            return Dog(
+                name=name,
+                breed=breed,
+                id=CURSOR.lastrowid
+            )
+
+        return Dog(
+            name=row[1],
+            breed=row[2],
+            id=row[0]
+        )
+
+    # Class Dog in dog.py contains a method "update()" that updates an instance's corresponding database record to match its new attribute values.
+    def update(self):
+        sql = """
+            UPDATE dogs
+            SET name = ?,
+                breed = ?
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed, self.id))
+        CONN.commit()  
